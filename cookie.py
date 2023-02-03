1275237b89e5b91e7839311a504afab305ae945a
Aleena P Mathews <aleenapmathews2004@gmail.com>
2023-02-03 21:52:36 +0530

    pgm

---
+++ cookie.py
@@ -0,0 +1,6 @@
+import random
+msg1="Today will be a lucky day"
+msg2= "You will meet someone new and interesting"
+msg3="Watch out for obstacles in your path"
+I=msg1,msg2,msg3
+print(random.choice(I))
